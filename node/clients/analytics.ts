ad302abae04104891b5b823b868fbf385e7e3d9e
reena-p <reena.p@echidnainc.com>
2022-02-21 13:41:35 +0530

    Included Clients

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,6 +1,7 @@
 import { AppClient, InstanceOptions, IOContext } from "@vtex/api";
 export default class Analytics extends AppClient {
   constructor(context: IOContext, options?: InstanceOptions) {
+    //intsall mocked analytics as it is a dependency of events-example
     super("vtex.mocked-analytics@0.x", context, options);
   }
   public getLiveUsers(): Promise<LiveUsersProduct[]> {
